33b4bf971ae4bfc921b5583dadb7820c02a35d69
Khaled <khaled.maged.200@gmail.com>
2025-07-04 22:41:21 +0300

    ok

--- main.py
+++ main.py
@@ -205,8 +205,6 @@ def get_whatsapp_media_bytes(media_id: str):
         return None, None
 
 def get_gemini_response(input_parts: list): # Removed generation_config parameter
-    client = genai.Client(api_key='GEMINI_API_KEY')
-
     response = client.models.generate_content(
             model='gemini-2.0-flash',
             contents=input_parts,
